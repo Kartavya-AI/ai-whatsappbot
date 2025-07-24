e6025967234fc69e60d449bc8d4ea6a475b62f8b
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2025-07-24 14:58:34 +0530

    push

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const qrcode = require('qrcode-terminal');
 const axios = require('axios');
 require('dotenv').config(); // Load environment variables from .env file
 // FastAPI endpoint configuration
-const FASTAPI_BASE_URL = process.env.FAST_API_BASE_URL || 'http://127.0.0.1:8000'; // Change this to your FastAPI server URL
+const FASTAPI_BASE_URL = process.env.FAST_API_BASE_URL || 'http://35.207.244.70:8000'; // Change this to your FastAPI server URL
 
 // Initialize WhatsApp client
 const client = new Client({
